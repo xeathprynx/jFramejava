274bbb0ac4778e5f2a1825c25756cd7f34c3505d
xeathprynx <xeathprynx@gmail.com>
2016-04-15 01:31:58 +0500

    Added files via upload

---
+++ Simplecalculator.java
@@ -0,0 +1,14 @@
+
+package simplecalculator;
+
+
+public class Simplecalculator {
+
+ 
+    public static void main(String[] args) {
+       
+        calculator c = new calculator();
+        c.show();
+    }
+    
+}
